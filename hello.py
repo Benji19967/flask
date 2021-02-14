9675c2331ca68811a4edd3551cad562d8fc3ff71
Benji19967 <benjamin.labrecque@mail.mcgill.ca>
2021-02-14 16:29:44 -0500

    Flask quickstart

---
+++ hello.py
@@ -0,0 +1,57 @@
+from flask import Flask, url_for, request
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Logging in"
+    else:
+        return "Showing you the logging form"
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
